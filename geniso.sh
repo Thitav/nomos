23a7c6696c8061c266dc207c265b839ce00bb925
Thitav <thiptav@gmail.com>
2024-05-26 21:14:19 -0300

    Changed project name

--- geniso.sh
+++ geniso.sh
@@ -1,5 +1,5 @@
 rm -r ./isodir
 mkdir -p ./isodir/boot/grub
-cp ./build/sysroot/usr/boot/kernel.bin ./isodir/boot/nomos.bin
+cp ./build/sysroot/usr/boot/kernel.bin ./isodir/boot/retros.bin
 cp ./grub.cfg ./isodir/boot/grub/grub.cfg
-grub-mkrescue -o ./isodir/nomos.iso ./isodir
+grub-mkrescue -o ./isodir/retros.iso ./isodir
